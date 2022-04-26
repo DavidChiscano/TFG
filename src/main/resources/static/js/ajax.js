ae20612d8c33e0651a779fa7b756aa778060f1cd
DavidChiscano <chiscano94@gmail.com>
2022-04-26 14:34:27 +0200

    buscar cuenta

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,9 +1,38 @@
-const RIOT_TOKEN = "RGAPI-a61bb82c-6877-4f5f-a78b-ae990f58b1cc";
-var nombre = '0kame';
-    const promise = fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nombre}`, {
-            headers: {"X-Riot-Token": RIOT_TOKEN}
-        }).then(res => res.json())
+const RIOT_TOKEN = "?api_key=RGAPI-f496b3dd-0b94-4807-9ee9-7e5caf0acd25";
+var nombre = '';
+var encryptedSummonerId = '';
+//URL's API RIOT'
+const urlNombreCuenta = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+const urlPerfil = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/';
+
+//BUSCAR INVOCADOR
+document.getElementById("btnBuscar").onclick = function() {
+	nombre = document.getElementById("buscador").value;
+	fetch(urlNombreCuenta + nombre + RIOT_TOKEN)
+		.then((resp) => resp.json())
+		.then(function(data) {
+			let nombreCuenta = document.getElementById("cuentaEncontrada");
+			let nivelCuenta = document.getElementById("nivelCuenta");
+			nombreCuenta.textContent = data.name;
+			nivelCuenta.textContent = data.summonerLevel;
+			data.id = encryptedSummonerId;
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+}
+
+//VER PERFIL DETALLADO
+document.getElementById("ver").onclick = function() {
+	console.log(encryptedSummonerId);
+	fetch(urlPerfil + encryptedSummonerId + RIOT_TOKEN)
+		.then((resp) => resp.json())
+		.then(function(data) {
+			console.log(data.tier);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
 
 
-
-
+}
